c3f0da3311a717ee90ca2b0870e632abb1901fed
hien <46805925+hien-nt@users.noreply.github.com>
2021-09-27 17:21:34 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,6 @@ set -e
 #   echo "S3_BUCKET is not set. Quitting."
 #   exit 1
 # fi
-# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-#   exit 1
-# fi
 
 # if [-z "$FILE"]; then
 #   echo "FILE is not set. Quitting"
@@ -24,16 +16,5 @@ set -e
 #   AWS_REGION="us-east-1"
 # fi
 
-mkdir -p ~/.aws
-
-touch ~/.aws/credentials
-
-echo "[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
-
 aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
   --region ${AWS_REGION} $*
-
-rm -rf ~/.aws
-
